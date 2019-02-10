c5e9b780f76a4765f593aa772a90a09da97323ec
Renata Svaboviciute <renata.svaboviciute@gmail.com>
2019-02-10 17:46:43 +0000

    creating new repository and moving the game

--- tetris.js
+++ tetris.js
@@ -3,11 +3,32 @@ const context = canvas.getContext('2d');
 
 context.scale(28, 28);
 
-const matrix = [
-  [0, 0, 0],
-  [1, 1, 0],
-  [0, 1, 1],
-];
+// const matrix = [
+//   [0, 0, 0],
+//   [1, 1, 0],
+//   [0, 1, 1],
+// ];
+
+//to clear the line(s) witch is filled with bricks
+function arenaSweep() {
+  let rowCount = 1;
+  //we check if there is a line without 0, continue till we find it
+  outer: for (let y = arena.length - 1; y > 0; --y) {
+    for (let x = 0; x < arena[y].length; ++x) {
+      if (arena[y][x] === 0) {
+        continue outer;
+      }
+    }
+    //we get the full row,fill it with zeros and place on top
+    const row = arena.splice(y, 1)[0].fill(0);
+    arena.unshift(row);
+    ++y;
+
+    player.lines += 1;
+    player.score += rowCount * 10;
+    rowCount *= 2;
+  }
+}
 
 //function detecting a collision
 function collide(arena, player) {
@@ -39,9 +60,55 @@ function createMatrix(w, h) {
   return matrix;
 }
 
+function createPiece(type) {
+  if (type === 'Z') {
+    return [
+        [1, 1, 0],
+        [0, 1, 1],
+        [0, 0, 0],
+    ];
+  } else if (type === 'O') {
+    return [
+        [2, 2],
+        [2, 2],
+    ];
+  } else if (type === 'I') {
+    return [
+        [0, 3, 0, 0],
+        [0, 3, 0, 0],
+        [0, 3, 0, 0],
+        [0, 3, 0, 0],
+    ];
+  } else if (type === 'S') {
+    return [
+        [0, 4, 4],
+        [4, 4, 0],
+        [0, 0, 0],
+    ];
+  } else if (type === 'L') {
+    return [
+        [0, 5, 0],
+        [0, 5, 0],
+        [0, 5, 5],
+    ];
+  } else if (type === 'J') {
+    return [
+        [0, 6, 0],
+        [0, 6, 0],
+        [6, 6, 0],
+    ];
+  } else if (type === 'T') {
+    return [
+        [7, 7, 7],
+        [0, 7, 0],
+        [0, 0, 0],
+    ];
+  }
+}
+
 //draw function
 function draw() {
-  context.fillStyle = '#3B1B2B';
+  context.fillStyle = '#0C1C2D';
   context.fillRect(0, 0, canvas.width, canvas.height);
 
   drawMatrix(arena, {x: 0, y: 0});""
@@ -52,7 +119,7 @@ function drawMatrix(matrix, offset) {
   matrix.forEach((row, y) => {
     row.forEach((value, x) => {
       if (value !== 0) {
-        context.fillStyle = 'purple';
+        context.fillStyle = colors[value];
         context.fillRect(x + offset.x,
                          y + offset.y,
                          1, 1);
@@ -78,7 +145,10 @@ function playerDrop() {
     //if collision occured - set player back to top
     player.pos.y--;
     merge(arena, player);
-    player.pos.y = 0;
+    playerReset();
+    arenaSweep();
+    updateLines();
+    updateScore();
   }
   dropCounter = 0;
 }
@@ -88,6 +158,25 @@ function playerMove(dir) {
   player.pos.x += dir;
   if (collide(arena, player)) {
     player.pos.x -= dir;
+  }
+}
+
+function playerReset() {
+  const pieces = 'ZOISLJT';
+  player.matrix = createPiece(pieces[pieces.length * Math.random() | 0]);
+  //we place player at the top
+  player.pos.y = 0;
+  //and in the middle
+  player.pos.x = (arena[0].length / 2 | 0) -
+                 (player.matrix[0].length / 2 | 0);
+  //to check if game is over(puzzles reached the top)
+  if (collide(arena, player)) {
+    //clear the arena to start again
+    arena.forEach(row => row.fill(0));
+    player.lines = 0;
+    player.score = 0;
+    updateLines();
+    updateScore();
   }
 }
 
@@ -135,6 +224,7 @@ let dropCounter = 0;
 let dropInterval = 1000;
 
 let lastTime = 0;
+
 //update function to draw continuously
 function update(time = 0) {
   const deltaTime = time - lastTime;
@@ -149,13 +239,34 @@ function update(time = 0) {
   requestAnimationFrame(update);
 }
 
+function updateLines() {
+  document.getElementById('lines').innerText = player.lines;
+}
+
+function updateScore() {
+  document.getElementById('score').innerText = player.score;
+}
+
+const colors = [
+  null,
+  'purple',
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'aqua'
+];
+
 //arena - the whole canvas area where we set width  with 12 units and hight with 20 units
 const arena = createMatrix(12, 20);
 
 //player structure that has a position and matrix
 const player = {
-  pos: {x: 2, y: 2},
-  matrix: matrix,
+  pos: {x: 0, y: 0},
+  matrix: null,
+  score: 0,
+  lines: 0,
 };
 
 document.addEventListener('keydown', event => {
@@ -166,13 +277,16 @@ document.addEventListener('keydown', event => {
     playerMove(1);
   } else if (event.keyCode === 40) {
     playerDrop();
-  } else if (event.keyCode === 81) {
+  } else if (event.keyCode === 65) {
     playerRotate(-1);
-  } else if (event.keyCode === 87) {
+  } else if (event.keyCode === 83) {
     playerRotate(1);
   }
 })
 
+playerReset();
+updateLines();
+updateScore();
 update();
 
 
